d5e1ca5e2dc134d683a0904ab50a2aa31e60cdcc
Andres Jimenez <ajthecoder1@gmail.com>
2025-04-08 18:23:35 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,26 +5,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let pronoun = ['no-one', 'himself', 'everything', 'what'];
+  let pronoun = ['noOne', 'himself', 'everything', 'what'];
   let adj = ['Huge', 'red', 'angry', 'tremendous'];
-  let noun = ['city', 'ice-cream', 'teacher', 'chair'];
+  let noun = ['city', 'iceCream', 'teacher', 'chair'];
 
   const domainNames = [];
-  for (let pronounIndex = 0; pronounIndex < pronoun.length; pronounIndex++){
-    const domainPronoun = pronoun[pronounIndex];
-    for (let adjIndex = 0; adjIndex < adj.length; adjIndex++){
-      const domainAdj = adj[adjIndex];
-      for (let nounIndex = 0; nounIndex < noun.length; nounIndex++){
-        const domainNoun = noun[nounIndex];
+  for (const domainPronoun of pronoun){
+    for (const domainAdj of adj){
+      for (const domainNoun of noun){
+        
         const domainName = domainPronoun + domainAdj + domainNoun;
         domainNames.push(domainName);
       }
     }
   }
-  console.log('List of domain names');
-  for (let i = 0; i < domainNames.length; i++){
-    console.log(domainNames[i] + '.com');
-  }
+console.log ('domain names'); domainNames.forEach(name => console.log(name + '.com'))
 
 
 
